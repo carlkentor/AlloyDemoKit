328f999fa52cb6e18708e8250f0a0d2bffa79350
Carl <carl.eriksson@soprasteria.com>
2019-04-04 11:16:50 +0200

    Vulcan setup

---
+++ src/VulcanEngine/Services/VulcanIndexModifier.cs
@@ -0,0 +1,38 @@
+﻿using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using System.Collections.Generic;
+using System.Linq;
+using TcbInternetSolutions.Vulcan.Core;
+
+namespace VulcanEngine.Services
+{
+    [ServiceConfiguration(typeof(IVulcanIndexingModifier), Lifecycle = ServiceInstanceScope.Singleton)]
+    public class VulcanIndexModifier : IVulcanIndexingModifier
+    {
+        private readonly List<IVulcanContentAncestorLoader> _vulcanContentAncestorLoaders;
+
+        public VulcanIndexModifier()
+        {
+        }
+        public void ProcessContent(IVulcanIndexingModifierArgs args)
+        {
+            // index ancestors
+            var ancestors = new List<ContentReference>();
+
+            // constructor injected service
+            if (_vulcanContentAncestorLoaders?.Any() == true)
+            {
+                foreach (var ancestorLoader in _vulcanContentAncestorLoaders)
+                {
+                    IEnumerable<ContentReference> ancestorsFound = ancestorLoader.GetAncestors(args.Content);
+
+                    if (ancestorsFound?.Any() == true)
+                    {
+                        ancestors.AddRange(ancestorsFound);
+                    }
+                }
+            }
+            args.AdditionalItems[VulcanFieldConstants.Ancestors] = ancestors.Select(x => x.ToReferenceWithoutVersion()).Distinct();
+        }
+    }
+}
